ec0d25ab6e1f66fd983264f486e07bed2ad88330
Luis Henrique <lhgmatias@gmail.com>
2025-05-06 11:56:57 -0300

    first commit

---
+++ cookin-up/src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+  
+export default {
+  components: { Banner }
+}
+</script>
+
+<template>
+  <Banner></Banner>
+</template>
+
+<style scoped>
+.titulo{
+  color: red;
+}
+</style>
